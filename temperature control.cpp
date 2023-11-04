67a84063d22afb17519342a40d3e05bb263ed999
ranasherif2004 <98807885+ranasherif2004@users.noreply.github.com>
2023-11-04 18:41:27 +0300

    Update temperature control.cpp

--- temperature control.cpp
+++ temperature control.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
@@ -7,8 +8,8 @@ int temperatureadjustment(int internaltemperature) {
     int tempadjustment = 25;
     if (internaltemperature >= temperaturemax)
     {
-        cout << "temperature exceed the max"<<endl;
-        cout << "temperature adjusted to" <<  "  " << tempadjustment;
+        cout << "temperature exceed the max" << endl;
+        cout << "temperature adjusted to" << "  " << tempadjustment;
     }
 
     else
@@ -19,9 +20,10 @@ int temperatureadjustment(int internaltemperature) {
 
 }
 int main() {
-    int internaltemp;
-    cout << "what is the internal temperature" << endl;
-    cin >> internaltemp;
+    
+    int internaltemp = rand() % 45;
+    cout << "internal temp=" << internaltemp;
     temperatureadjustment(internaltemp);
+
 
 }
