aded02af6be4b602c20b18e99ecd87df9f2dce88
ranasherif2004 <98807885+ranasherif2004@users.noreply.github.com>
2023-11-05 02:37:17 +0300

    Add files via upload

---
+++ firedepatment after 2 minutes.cpp
@@ -0,0 +1,50 @@
+
+
+#include <iostream>
+#include <chrono>
+#include<thread>
+
+using namespace std;
+string firedepartmntalarm(string response) {
+    if (response.empty()) {
+
+       return "call ";
+
+    }
+    else if (response == "ok") {
+      return  "cancel";
+
+    }
+
+}
+int main() {
+    
+
+    string response;
+    cout << "put your response within 2 minutes"<<endl;
+    cin >> response;
+
+
+
+string x= firedepartmntalarm(response);
+    
+    
+
+
+    if (x == "call")
+        cout << "fire department called";
+    else if (x == "cancel")
+        cout << "user responded";
+
+
+    return 0;
+
+
+    this_thread::sleep_for(chrono::seconds(10));
+
+    
+
+
+}
+
+
