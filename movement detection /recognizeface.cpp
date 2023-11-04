7d198e77e5457e436979fe5d7ce33399ed9aef4f
Samakhaled18 <112958837+Samakhaled18@users.noreply.github.com>
2023-11-04 22:20:04 +0200

    Update recognizeface.cpp

--- movement detection /recognizeface.cpp
+++ movement detection /recognizeface.cpp
@@ -3,21 +3,18 @@
 bool randomBool() {
    return rand() > (RAND_MAX/2);
 }
-// Function to detect motion
+
 bool checkMovement() {
     return randomBool();
-    // Code to detect motion using sensors
-    // Return true if motion is detected, false otherwise
+    
 }
 
-// Function to recognize face
 bool recognizeFace() {
     return randomBool();
-    // Code to recognize face using facial recognition algorithm
-    // Return true if face is recognized, false otherwise
+    
 }
 
-// Function to alert user
+
 void alertUser() {
     std::cout << "Motion detected! Unidentified person outside the house." << std::endl;
     std::cout << "Do you want to call the authorities? (Y/N): ";
@@ -26,7 +23,7 @@ void alertUser() {
     std::cin >> choice;
 
     if (choice == 'Y' || choice == 'y') {
-        // Code to call authorities
+        
         std::cout << "Calling authorities..." << std::endl;
     } else {
         std::cout << "Ignoring the alert." << std::endl;
