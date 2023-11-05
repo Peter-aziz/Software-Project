f37124a26b2d2873ed3be1340d9739e67536ee62
ranasherif2004 <98807885+ranasherif2004@users.noreply.github.com>
2023-11-05 11:08:36 +0300

    Add files via upload

---
+++ firedepartment.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<chrono>
+#include<thread>
+using namespace std;
+
+
+
+
+
+
+
+int main() {
+	
+	auto seconds = 120s;
+	string response;
+	cout << "respond with 2 minutes" << endl;
+
+
+	auto start = chrono::steady_clock::now();
+	while (true) {
+		this_thread::sleep_for(seconds);
+		if (chrono::steady_clock::now() - start == chrono::seconds(seconds)) {
+
+
+			cout << "fire department called";
+			break;
+		}
+		else if (cin >> response) {
+			if (response == "ok") {
+				cout << "no action needed";
+			}
+		}
+	}
+		//int durationSeconds = 50;
+	//string response;
+
+
+
+	//auto start = chrono::steady_clock::now();
+
+	//this_thread::sleep_for(chrono::seconds(durationSeconds));
+
+	//auto end = chrono::steady_clock::now();
+
+	//chrono::duration<double> elapsedSeconds = end - start;
+
+	//
+	////cout << "Timer expired after " << elapsedSeconds.count() << " seconds." << endl;
+
+
+	//if (elapsedSeconds.count() < 50) {
+	//	cout << "respond with 2 minutes" << endl;
+	//	cin >> response;
+	//	if (response == "ok") {
+	//		cout<< "no action needed";
+	//	}
+	//}
+	//else
+
+	//	cout << "fire department called";
+
+
+	} 
+	
