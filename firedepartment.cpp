1ca429319c00206696d944e0bf82febdb328a630
Peter-aziz <99272427+Peter-aziz@users.noreply.github.com>
2023-12-05 21:17:42 +0200

    Update firedepartment.cpp

--- firedepartment.cpp
+++ firedepartment.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 int main() {
 	
-	auto seconds = 120s;
+	auto seconds = 60s;
 	string response;
-	cout << "respond with 2 minutes" << endl;
+	cout << "respond within 1 minute" << endl;
 
 	this_thread::sleep_for(seconds);
 	auto start = chrono::steady_clock::now();
@@ -31,34 +31,4 @@ int main() {
 			}
 		}
 	}
-		//int durationSeconds = 50;
-	//string response;
-
-
-
-	//auto start = chrono::steady_clock::now();
-
-	//this_thread::sleep_for(chrono::seconds(durationSeconds));
-
-	//auto end = chrono::steady_clock::now();
-
-	//chrono::duration<double> elapsedSeconds = end - start;
-
-	//
-	////cout << "Timer expired after " << elapsedSeconds.count() << " seconds." << endl;
-
-
-	//if (elapsedSeconds.count() < 50) {
-	//	cout << "respond with 2 minutes" << endl;
-	//	cin >> response;
-	//	if (response == "ok") {
-	//		cout<< "no action needed";
-	//	}
-	//}
-	//else
-
-	//	cout << "fire department called";
-
-
-	} 
 	
