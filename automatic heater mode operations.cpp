c3b81b106a7999a99bdef6ba0d120352a4c38237
aishawaelll <75455904+aishawaelll@users.noreply.github.com>
2023-12-05 15:43:48 +0200

    Update automatic heater mode operations.cpp

--- automatic heater mode operations.cpp
+++ automatic heater mode operations.cpp
@@ -4,12 +4,25 @@
 using namespace std;
 
 double internalTemp;
+double customHeaterTemp;
+double tempWhenTooCold;
+
+void CustomizeHeaterSettings()
+{
+    cout<<"Please choose the external temperature at which you would like to turn the house's heater on:  "; 
+    cin>>tempWhenTooCold;
+    cout<<endl;
+    
+    cout<<"Now please determine heater mode, which is the temperature (in degrees Celsius) you would like to set the heater to when the house is too cold: ";
+    cin>>customHeaterTemp;
+    
+}
 
 bool heaterMode (float externalTemperature)
 { 
-    if (externalTemperature < 10)
+    if (externalTemperature <= tempWhenTooCold)
     {
-        internalTemp = 40;
+        internalTemp = customHeaterTemp;
         return true;
     }
         return false;
@@ -18,9 +31,10 @@ bool heaterMode (float externalTemperature)
 
 int main() 
 {
+    CustomizeHeaterSettings();
     float externalTemperature = rand () % 55;
     if (heaterMode(externalTemperature))
-        cout<<"Heater Mode activated (40 degrees Celsius)";
+        cout<<"Heater Mode activated ("<<customHeaterTemp<<" degrees Celsius)";
     else
         cout<<"Current temperature is " << internalTemp;
 }
