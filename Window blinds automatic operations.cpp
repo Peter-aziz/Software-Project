027ef995dce1266d417c57876f16a09b3b0a6fbe
aishawaelll <75455904+aishawaelll@users.noreply.github.com>
2023-12-05 15:44:40 +0200

    Update Window blinds automatic operations.cpp

--- Window blinds automatic operations.cpp
+++ Window blinds automatic operations.cpp
@@ -3,17 +3,39 @@
 #include <ctime>
 using namespace std;
 
-int percentageWindowBlindIsOpen (float solarIrradiance)
+int currentUVIndex;
+
+int percentageWindowBlindIsOpen (int upperLim, int LowerLim, int percentCloseBlinds)
 {
-    if (solarIrradiance >= 1350 || solarIrradiance <= 1000)
-        return 0;
-    else 
-        return 75;
+    if (currentUVIndex >= LowerLim || currentUVIndex <= upperLim)
+        return percentCloseBlinds;
+    return 0;
+}
+
+void customWindowBlindsSettings(int roomNumber)
+{
+    cout<<"Please choose the UV index range and how much you'd like to open/close the window blinds accordingly: "<<endl;
+    int UVUpperLim, UVLowerLim;
+    cin>>UVUpperLim>>UVLowerLim;
+    
+    int percentCloseBlinds;
+    cin>>percentCloseBlinds;
+    percentageWindowBlindIsOpen (UVUpperLim, UVLowerLim, percentCloseBlinds);
+}
+
+void blindsLocation()
+{
+    cout<<"There are blinds located at each of the following rooms: \n 1. Kitchen \n 2. Living Room \n 3. Bedroom 1 \n 4. Bedroom 2 \n 5. Bathroom \n 6. Dining "<<endl;
+    cout<<"Which room would you like to adjust the automatic blinds settings for?"<<endl;
+    int roomNum;
+    cin>>roomNum;
+    customWindowBlindsSettings(roomNum);
 }
 
 int main()
 {
-	float solarIrradiance = rand () % 2000;
-	int windowBlindsOpen = percentageWindowBlindIsOpen (solarIrradiance);
-	cout<<"Current Solar Irradiance is: " << solarIrradiance<<", automatically opening/closing the blinds " << windowBlindsOpen<<"%";
+    currentUVIndex = rand () % 11;
+    cout<<"First choosing the blinds location whose settings the user wishes to adjust: "<<endl;
+    blindsLocation();
+    cout<<"Current UV Index is: " << currentUVIndex<<", automatically opening/closing the blinds ";
 }
